efd416f26fa9d9d6202a1d5daa775bb2d240f407
zwerek1990 <zwerek1990@gmail.com>
2013-09-18 10:51:14 +0400

    Open Sans шрифт починил треугольик в галереи товаров (overflow:hidden)
    Signed-off-by: zwerek1990 <zwerek1990@gmail.com>

--- components/head.php
+++ components/head.php
@@ -9,13 +9,16 @@
 
 <title>Чебоксарский трикотаж. Официальный сайт Чебоксарской трикотажной фабрики</title>
 <script src="js/libs/jquery-1.10.2.js"></script>
-<link href='http://fonts.googleapis.com/css?family=Didact+Gothic&subset=cyrillic,latin' rel='stylesheet' type='text/css'>
+<link href="/style/GFont2/GFont2.css" type="text/css" media="all" rel="stylesheet">
+
+
 <!-- fancybox -->
 <script src="/js/libs/fancybox/jquery.mousewheel-3.0.6.pack.js"></script>
 <link rel="stylesheet" href="/js/libs/fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
 <script type="text/javascript" src="/js/libs/fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
 <link rel="stylesheet" href="/js/libs/fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
 <script type="text/javascript" src="/js/libs/fancybox/source/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
+
 
 <script src="js/libs/jquery.blueberry.js"></script>
 
